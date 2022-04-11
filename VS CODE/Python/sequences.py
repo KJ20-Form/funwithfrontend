f91ed17c2991564daac34c82606a82f7f937cf8d
Kenneth Jaehne <kenjaehne@gmail.com>
2022-04-11 17:20:21 -0400

    first

---
+++ VS CODE/Python/sequences.py
@@ -0,0 +1,17 @@
+name = "Harry"
+
+print(name[1])
+
+names= ["Harry", "Ron", "Hermione"]
+print([2])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+#list sequence of mutable values
+#tuple sequence of immutable values
+#set collection of unique values
+#dict collection of key-value pairs
+
