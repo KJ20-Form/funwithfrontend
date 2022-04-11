2b853be3e1ceca222b9d76bdbcc0354f7b57b9c6
Kenneth Jaehne <kenjaehne@gmail.com>
2022-04-11 17:35:17 -0400

    python minus dictionaries

---
+++ VS CODE/Python/loops.py
@@ -0,0 +1,17 @@
+for i in [1,2,3,4,5]:
+    print(i)
+    
+for i in range(6):
+    print(i)
+    
+names = ["Harry", "Ron", "Hermione"]
+
+for name in names:
+    print(name)
+    
+harry= "Harry"
+
+for character in harry:
+    print(character)
+
+
