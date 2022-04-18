162c4092eb06c007cc939e9ce587a67d74fe927b
Kenneth Jaehne <kenjaehne@gmail.com>
2022-04-17 22:32:00 -0400

    Easter #2

--- VS CODE/Python/prime.py
+++ VS CODE/Python/prime.py
@@ -1,8 +1,8 @@
 import math
 def is_prime(n):
-    if n<2:
+    if n < 2:
         return False
-    for i in range(2, int(math.sqrt(n))): 
+    for i in range(2, int(math.sqrt(n)) +1): 
         if n % i == 0:
             return False
     return True
